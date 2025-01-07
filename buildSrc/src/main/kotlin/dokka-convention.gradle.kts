a86e0d52b43eb654a8448c261973c2834113e3ab
Yongshun Shreck Ye <ShreckYe@gmail.com>
2025-01-07 19:52:44 +0800

    Add some more comments, which should've been added in commit 83ec57eb65e0f72b3f6c2b17c7811ede07218bc6

--- buildSrc/src/main/kotlin/dokka-convention.gradle.kts
+++ buildSrc/src/main/kotlin/dokka-convention.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     id("com.huanshankeji.team.dokka.github-dokka-convention")
 }
 
+// TODO use or remove
+// This does not resolve since commit bc5afe61d2dba3a8cef51d66ea60feeeb3e83093.
 /*
 dokka {
     // https://github.com/Kotlin/dokka/issues/3885#issuecomment-2449645480
