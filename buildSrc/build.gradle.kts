f9e871ba614aa3b1227a7c2392e95ce1244da063
Shihao Xiang <shihaoxiangcth@gmail.com>
2025-06-07 11:01:09 +0200

    Change: ------------------ buildSrc kotlinVersion -> 2.2.0-RC2 compose-gradle-plugin -> 1.8.1 -------------------- ------------------ demo Copy -> Sync val jsBrowserDistribution by tasks.getting(Sync::class) val wasmJsBrowserDistribution by tasks.getting(Sync::class) -------------------- ------------------ material-icons-core comment the material-icons-extended dependencies --------------------

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -10,10 +10,10 @@ repositories {
 }
 
 dependencies {
-    val kotlinVersion = "2.1.0"
+    val kotlinVersion = "2.2.0-RC2"
     implementation(kotlin("gradle-plugin", kotlinVersion))
     implementation("org.jetbrains.kotlin:compose-compiler-gradle-plugin:$kotlinVersion")
-    implementation("org.jetbrains.compose:compose-gradle-plugin:1.7.3")
+    implementation("org.jetbrains.compose:compose-gradle-plugin:1.8.1")
     implementation("com.huanshankeji.team:gradle-plugins:0.9.0") // don't use a snapshot version in a main branch
     implementation("com.android.tools.build:gradle:8.7.3")
     implementation("com.huanshankeji:common-gradle-dependencies:0.9.0-20241203") // don't use a snapshot version in a main branch
