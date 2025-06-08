db7c876f7f9f1dfdf0682e6fc11d9f056a32e0a4
Yongshun Shreck Ye <ShreckYe@gmail.com>
2025-06-08 17:12:11 +0800

    Run the AGP Upgrade Assistant
    Potential errors like this should be resolved:
    
    ```
    e: file:///home/runner/.gradle/caches/modules-2/files-2.1/org.jetbrains.kotlin/kotlin-stdlib/2.2.0-RC2/87c573e16fe0194e3dba3f890597bb372c2b83f6/kotlin-stdlib-2.2.0-RC2.jar!/META-INF/kotlin-stdlib-jdk7.kotlin_module Module was compiled with an incompatible version of Kotlin. The binary version of its metadata is 2.2.0, expected version is 2.0.0.
    ```

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -15,6 +15,6 @@ dependencies {
     implementation("org.jetbrains.kotlin:compose-compiler-gradle-plugin:$kotlinVersion")
     implementation("org.jetbrains.compose:compose-gradle-plugin:1.8.1")
     implementation("com.huanshankeji.team:gradle-plugins:0.9.0") // don't use a snapshot version in a main branch
-    implementation("com.android.tools.build:gradle:8.7.3")
+    implementation("com.android.tools.build:gradle:8.10.1")
     implementation("com.huanshankeji:common-gradle-dependencies:0.9.0-20241203") // don't use a snapshot version in a main branch
 }
