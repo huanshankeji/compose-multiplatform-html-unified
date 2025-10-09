03f854aac94ed0462625041b1cf5e478f2391919
Yongshun Ye <ShreckYe@gmail.com>
2025-10-09 15:30:21 +0800

    Partially revert "Try bumping all dependencies to the latest (including snapshots for our own projects)" to revert snapshot dependencies to stable
    This partially reverts commit f4e2d0a719382e177c2e81c5ac8e47f5e8b0c7dd.

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -14,7 +14,7 @@ dependencies {
     implementation(kotlin("gradle-plugin", kotlinVersion))
     implementation("org.jetbrains.kotlin:compose-compiler-gradle-plugin:$kotlinVersion")
     implementation("org.jetbrains.compose:compose-gradle-plugin:1.9.0")
-    implementation("com.huanshankeji.team:gradle-plugins:0.10.0-SNAPSHOT") // TODO don't use a snapshot version in a main branch
+    implementation("com.huanshankeji.team:gradle-plugins:0.9.0") // don't use a snapshot version in a main branch
     implementation("com.android.tools.build:gradle:8.12.3")
-    implementation("com.huanshankeji:common-gradle-dependencies:0.10.0-20250918-SNAPSHOT") // TODO don't use a snapshot version in a main branch
+    implementation("com.huanshankeji:common-gradle-dependencies:0.9.0-20241203") // don't use a snapshot version in a main branch
 }
