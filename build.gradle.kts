c88bd7cbf4df444c5741790d45ea54885cedaea4
Yongshun Ye <ShreckYe@gmail.com>
2025-10-09 16:59:34 +0800

    Delete more

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,3 @@
 tasks.wrapper {
     distributionType = Wrapper.DistributionType.ALL
 }
-
-plugins {
-    id("org.jetbrains.kotlinx.binary-compatibility-validator") version "0.18.1"
-}
-
-apiValidation {
-    @OptIn(kotlinx.validation.ExperimentalBCVApi::class)
-    klib {
-        enabled = true
-    }
-}
