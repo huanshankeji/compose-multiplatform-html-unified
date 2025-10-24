ad092fcd6bb5ba724be56ee87dd1711cfbecc1d1
Yongshun Ye <ShreckYe@gmail.com>
2025-10-24 19:37:54 +0800

    Bump all dependencies to the latest and add release reference links

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -10,11 +10,12 @@ repositories {
 }
 
 dependencies {
-    val kotlinVersion = "2.2.10"
+    val kotlinVersion = "2.2.21"
     implementation(kotlin("gradle-plugin", kotlinVersion))
     implementation("org.jetbrains.kotlin:compose-compiler-gradle-plugin:$kotlinVersion")
-    implementation("org.jetbrains.compose:compose-gradle-plugin:1.9.0")
+    implementation("org.jetbrains.compose:compose-gradle-plugin:1.9.1")
     implementation("com.huanshankeji.team:gradle-plugins:0.10.0-SNAPSHOT") // TODO don't use a snapshot version in a main branch
+    // https://developer.android.com/build/releases/gradle-plugin, https://mvnrepository.com/artifact/com.android.tools.build/gradle
     implementation("com.android.tools.build:gradle:8.12.3")
-    implementation("com.huanshankeji:common-gradle-dependencies:0.10.0-20250918-SNAPSHOT") // TODO don't use a snapshot version in a main branch
+    implementation("com.huanshankeji:common-gradle-dependencies:0.10.0-20251024-SNAPSHOT") // TODO don't use a snapshot version in a main branch
 }
--- buildSrc/src/main/kotlin/VersionsAndDependencies.kt
+++ buildSrc/src/main/kotlin/VersionsAndDependencies.kt
@@ -8,11 +8,18 @@ val commonDependencies = CommonDependencies()
 object DependencyVersions {
     const val composeMultiplatform = ComposeBuildConfig.composeVersion // for "ui-unit"
 
+
     // https://slack-chats.kotlinlang.org/t/27579160/after-updating-to-1-8-0-rc01-package-androidx-compose-materi
     // https://github.com/JetBrains/compose-multiplatform/releases/tag/v1.8.0-rc01
     const val composeMultiplatformMaterialIconsExtended = "1.7.3"
+
+    // https://github.com/varabyte/kobweb/releases
     val kobweb = "0.23.3"
     val huanshankejiComposeHtml = "0.4.1-SNAPSHOT" // TODO don't use a snapshot version in a main branch
+
+    // https://github.com/mpetuska/kmdc/releases
     val kmdc = "0.1.2"
-    val materialSymbols = "0.35.2"
+
+    // https://github.com/marella/material-symbols/releases
+    val materialSymbols = "0.38.0"
 }
