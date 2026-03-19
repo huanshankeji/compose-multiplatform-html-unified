aaa2d5a875daf6ec6125ec88d2be435bc36bc0e8
Yongshun Ye <ShreckYe@gmail.com>
2026-03-19 20:55:46 +0800

    Review and improve the KDoc for `Scaffold`

--- material3/src/commonMain/kotlin/com/huanshankeji/compose/material3/Scaffold.kt
+++ material3/src/commonMain/kotlin/com/huanshankeji/compose/material3/Scaffold.kt
@@ -2,6 +2,7 @@ package com.huanshankeji.compose.material3
 
 import androidx.compose.runtime.Composable
 import com.huanshankeji.compose.foundation.layout.PaddingValues
+import com.huanshankeji.compose.foundation.layout.padding
 import com.huanshankeji.compose.ui.Modifier
 
 /*
@@ -27,11 +28,11 @@ enum class FabPosition {
  * @param bottomBar bottom bar of the screen, typically a navigation bar
  * @param snackbarHost component to host [Snackbar]s that are pushed to be shown via [SnackbarHostState.showSnackbar],
  * typically a [SnackbarHost]
- * @param floatingActionButton Main action button of the screen, typically a [com.huanshankeji.compose.material3.FloatingActionButton]
+ * @param floatingActionButton Main action button of the screen, typically a [FloatingActionButton]
  * @param floatingActionButtonPosition position of the FAB on the screen. See [FabPosition].
  * @param content content of the screen. The lambda receives a [PaddingValues] that should be applied to the content root
- * via [com.huanshankeji.compose.foundation.layout.padding] to properly offset top and bottom bars.
- * Conventional values are 64 DP for top and bottom bars if they exist and 0 for horizontal padding on Compose UI,
+ * via [padding] to properly offset top and bottom bars.
+ * Conventional values are 64 dp for top and bottom bars if they exist and 0 for horizontal padding on Compose UI,
  * and 0 for all sides on JS DOM.
  * According to the corresponding doc for `androidx.compose.material3.Scaffold`, if the content is scrollable,
  * such values to be applied to "the child of the scroll, and not on the scroll itself".
