dc0eb0ab148120de8ed67d0bd8d73055618cea6c
Yongshun Ye <ShreckYe@gmail.com>
2026-03-25 21:06:44 +0800

    Add a comment for https://github.com/huanshankeji/compose-multiplatform-html-unified/pull/123#discussion_r2987975747

--- material-icons/core/src/jsMain/kotlin/com/huanshankeji/compose/material/icons/Icons.js.kt
+++ material-icons/core/src/jsMain/kotlin/com/huanshankeji/compose/material/icons/Icons.js.kt
@@ -1,3 +1,4 @@
 package com.huanshankeji.compose.material.icons
 
+// TODO make it a value class to save some memory when it's supported by Kotlin or just make the Compose UI impl a value class too
 actual class Icon(val name: String)
