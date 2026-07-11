cd8b1051c90a2c9752b14a6cf4dab3d6a9e1f4b7
Yongshun Ye <shreckye@gmail.com>
2026-07-12 01:28:31 +0800

    Tighten pluginManagement: drop redundant mavenLocal and allow dirty/SNAPSHOT suffixes on -dev-commit- versions.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,5 @@
 pluginManagement {
     repositories {
-        mavenLocal()
         gradlePluginPortal()
         google()
         exclusiveContent {
@@ -22,7 +21,7 @@ pluginManagement {
                 }
             }
             filter {
-                includeVersionByRegex("com\\.huanshankeji", ".*", ".*-dev-commit-[0-9a-f]+$")
+                includeVersionByRegex("com\\.huanshankeji", ".*", ".*-dev-commit-[0-9a-f]+.*")
             }
         }
     }
